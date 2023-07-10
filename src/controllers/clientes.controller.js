5a4593f4575878dccd02d9c557aea6d51c02b55b
Félix Merchán <felix.merchan.92@gmail.com>
2023-07-09 19:21:23 -0500

    Establecí la conexion pg en el controlador clientes

---
+++ src/controllers/clientes.controller.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: '35.239.172.154',
+    user: 'rootdace',
+    password: 'dace1234',
+    database: 'callcenter',
+    port: '5432'
+});
